bac862e1b98e278c7c5a6346aa62c7dd388acd24
PElumalai <elumalaiperiasamy@gmail.com>
2019-02-13 18:59:57 +0530

    Function definition made in modulesandpackages.py file

--- 13-02-2019/file1.py
+++ 13-02-2019/file1.py
@@ -1,3 +1,4 @@
+#Function definition
 def myfun(x):
     return [ num for num in range(x) if num%2==0]
 list1=myfun(20)
--- 13-02-2019/file3.py
+++ 13-02-2019/file3.py
@@ -1,6 +1,8 @@
 import sys
+# sys is a libray in python, system input output operations
 import file1
 
 num=(sys.argv[0])
+#argv[0] to print file name, argv[1] gives the input parameter i.e !python files3.py 100
 print(num)
 #print(file1.myfun(num))
